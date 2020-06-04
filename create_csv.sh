0930f81e27b147d2c73fd59e53ee5817ae759a38
Paul Hausner <pauleon2410@gmail.com>
2020-06-04 22:22:01 +0000

    add script that creates csv of current files in HDFS and then stores it also in HDFS

---
+++ create_csv.sh
@@ -0,0 +1,10 @@
+hadoop fs -rm /LDSA/subset/files.csv;
+hadoop fs -ls -R /LDSA/dataset | grep '.h5$'| python -c '
+import sys
+with open("/home/ubuntu/files.csv", "w") as myfile:
+    for line in sys.stdin:
+        r = line.strip("\n").split(None, 10)
+        fn = r.pop()
+        myfile.write(",".join(r) + ",\"" + fn.replace("\"", "\"\"") + "\"")
+        myfile.write("\n")
+' && hdfs dfs -put /home/ubuntu/files.csv /LDSA/dataset && rm /home/ubuntu/files.csv && echo "current list of files succesfully created saved on hdfs"
